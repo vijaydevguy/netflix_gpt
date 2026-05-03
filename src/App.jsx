f75f73492717d25f42d10298ff51999879f1e300
vijaydevguy <vijayakumarjob01@gmail.com>
2026-05-03 12:53:29 +0530

    speed_insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
+import { SpeedInsights } from "@vercel/speed-insights";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <SpeedInsights />
       Staring project
       <h2 className="text-[48px] font-bold text-red-600">Title</h2>
     </>
